270ef545dd7badebebb4154d4e94a5c94c7ac9e3
jeffery graham <jeffery.lemar.graham@gmail.com>
2020-09-17 20:35:43 -0400

    group items by list id

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Item from './Item';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: [],
+    };
+  }
+
+  componentDidMount() {
+    //This API enables cross-origin requests to anywhere.
+    var proxyUrl = 'https://cors-anywhere.herokuapp.com/',
+      targetUrl = 'https://fetch-hiring.s3.amazonaws.com/hiring.json';
+    fetch(proxyUrl + targetUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({ data: [...data] });
+      });
+  }
+
+  render() {
+    //filter items and sort by list if & name
+    const items = this.state.data
+      .filter((item) => item.name)
+      .sort(function (a, b) {
+        return a.listId - b.listId || +a.name.slice(5) - +b.name.slice(5);
+      });
+
+    //get unique list ids for grouping items
+    const listIds = [...new Set(items.map((data) => data.listId))];
+
+    return (
+      <div className='App'>
+        {listIds.map((listId) => (
+          <div key={listId}>
+            <h2>List Id: {listId}</h2>
+            {items.map(
+              (item) =>
+                listId === item.listId && <Item key={item.id} item={item} />
+            )}
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
